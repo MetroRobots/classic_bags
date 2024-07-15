00c5f7788db8601bfc3f276a1860e895b6d14e40
David V. Lu!! <davidvlu@gmail.com>
2024-07-15 10:31:11 -0400

    Add topic_id to Python Metadata (#17)

--- classic_bags/__init__.py
+++ classic_bags/__init__.py
@@ -90,7 +90,8 @@ class Bag:
 
         if topic not in self.topic_metadata:
             msgtype = get_message_name(msg)
-            self.topic_metadata[topic] = TopicMetadata(name=topic, type=msgtype,
+            topic_id = len(self.topic_metadata)
+            self.topic_metadata[topic] = TopicMetadata(id=topic_id, name=topic, type=msgtype,
                                                        serialization_format=self.serialization_format)
             self.writer.create_topic(self.topic_metadata[topic])
 
